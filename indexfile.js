7d7180fed015eedb3ef8beb71f6d831a885aa27f
Jamal Ruiz <jamalruiz@jamals-air-2.mynetworksettings.com>
2022-06-12 11:08:58 -0400

    first commit

--- indexfile.js
+++ indexfile.js
@@ -1,8 +1,8 @@
-function fn(...c) {
+function AllNumbers(...c) {
   if (!c.every((a) => Number.isNumber(a)))
     throw "All arguments must be numbers.";
   return c.reduce((a, b) => a + b);
 }
 
-fn(1, 2, 3); //> 6
-fn(10, "B", 20); //> error All arguments must be numbers.
+AllNumbers(1, 2, 3); //> 6
+AllNumbers(10, "B", 20); //> error All arguments must be numbers.
