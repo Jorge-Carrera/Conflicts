4bf9a3463f8c2d14b96541e6adbe7e9a5e026a4b
Jorge Carrera <JCarrera994@gmail.com>
2022-06-12 11:07:56 -0400

    updates variables

--- indexfile.js
+++ indexfile.js
@@ -1,8 +1,10 @@
-function fn(...c) {
-  if (!c.every((a) => Number.isNumber(a)))
+function fn(...array) {
+  if (!array.every((a) => Number.isNumber(a)))
     throw "All arguments must be numbers.";
-  return c.reduce((a, b) => a + b);
+  return array.reduce((a, b) => a + b);
 }
 
 fn(1, 2, 3); //> 6
-fn(10, "B", 20); //> error All arguments must be numbers.
+
+
+fn(10, "B", 20); //>  shoul return error All arguments must be numbers.
